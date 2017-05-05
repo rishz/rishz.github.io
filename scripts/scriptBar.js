b6a58d734b99ec8491096b073b63b174312112c3
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-05-05 16:16:27 +0530

    modifications

--- scripts/scriptBar.js
+++ scripts/scriptBar.js
@@ -2,82 +2,92 @@
  * Created by rishabhshukla on 04/05/17.
  */
 
-$(document).ready(function(){
-
-    /**
-     * This object controls the nav bar. Implement the add and remove
-     * action over the elements of the nav bar that we want to change.
-     *
-     * @type {{flagAdd: boolean, elements: string[], add: Function, remove: Function}}
-     */
-    var myNavBar = {
-
-        flagAdd: true,
-
-        elements: [],
-
-        init: function (elements) {
-            this.elements = elements;
-        },
-
-        add : function() {
-            if(this.flagAdd) {
-                for(var i=0; i < this.elements.length; i++) {
-                    document.getElementById(this.elements[i]).className += " fixed-theme";
-                }
-                this.flagAdd = false;
-            }
-        },
-
-        remove: function() {
-            for(var i=0; i < this.elements.length; i++) {
-                document.getElementById(this.elements[i]).className =
-                    document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
-            }
-            this.flagAdd = true;
-        }
-
-    };
-
-    /**
-     * Init the object. Pass the object the array of elements
-     * that we want to change when the scroll goes down
-     */
-    myNavBar.init(  [
-        "header",
-        "header-container",
-        "brand"
-    ]);
-
-    /**
-     * Function that manage the direction
-     * of the scroll
-     */
-    function offSetManager(){
-
-        var yOffset = 0;
-        var currYOffSet = window.pageYOffset;
-
-        if(yOffset < currYOffSet) {
-            myNavBar.add();
-        }
-        else if(currYOffSet == yOffset){
-            myNavBar.remove();
-        }
-
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
     }
-
-    /**
-     * bind to the document scroll detection
-     */
-    window.onscroll = function(e) {
-        offSetManager();
-    };
-
-    /**
-     * We have to do a first detectation of offset because the page
-     * could be load with scroll down set.
-     */
-    console.log('WORKING');
-    offSetManager();
 });
+
+//
+// $(document).ready(function(){
+//
+//     /**
+//      * This object controls the nav bar. Implement the add and remove
+//      * action over the elements of the nav bar that we want to change.
+//      *
+//      * @type {{flagAdd: boolean, elements: string[], add: Function, remove: Function}}
+//      */
+//     var myNavBar = {
+//
+//         flagAdd: true,
+//
+//         elements: [],
+//
+//         init: function (elements) {
+//             this.elements = elements;
+//         },
+//
+//         add : function() {
+//             if(this.flagAdd) {
+//                 for(var i=0; i < this.elements.length; i++) {
+//                     document.getElementById(this.elements[i]).className += " fixed-theme";
+//                 }
+//                 this.flagAdd = false;
+//             }
+//         },
+//
+//         remove: function() {
+//             for(var i=0; i < this.elements.length; i++) {
+//                 document.getElementById(this.elements[i]).className =
+//                     document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
+//             }
+//             this.flagAdd = true;
+//         }
+//
+//     };
+//
+//     /**
+//      * Init the object. Pass the object the array of elements
+//      * that we want to change when the scroll goes down
+//      */
+//     myNavBar.init(  [
+//         "header",
+//         "header-container",
+//         "brand"
+//     ]);
+//
+//     /**
+//      * Function that manage the direction
+//      * of the scroll
+//      */
+//     function offSetManager(){
+//
+//         var yOffset = 0;
+//         var currYOffSet = window.pageYOffset;
+//
+//         if(yOffset < currYOffSet) {
+//             myNavBar.add();
+//         }
+//         else if(currYOffSet == yOffset){
+//             myNavBar.remove();
+//         }
+//
+//     }
+//
+//     /**
+//      * bind to the document scroll detection
+//      */
+//     window.onscroll = function(e) {
+//         offSetManager();
+//     };
+//
+//     /**
+//      * We have to do a first detectation of offset because the page
+//      * could be load with scroll down set.
+//      */
+//     console.log('WORKING');
+//     offSetManager();
+// });
